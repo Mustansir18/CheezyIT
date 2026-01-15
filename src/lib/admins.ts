a840502d33d6af63c576a2c541ce953314d5ab00
Mustansir Ali <mustansir133@gmail.com>
2026-01-15 19:44:23 +0000

    MTuyKiLxruWCp3BtZI2NYAsfGdD2 make this user as a admin

---
+++ src/lib/admins.ts
@@ -0,0 +1,13 @@
+'use server';
+
+// This file contains the list of user UIDs that have admin privileges.
+// You can add more UIDs to this array to grant admin access to other users.
+
+export const ADMIN_UIDS = [
+  'MTuyKiLxruWCp3BtZI2NYAsfGdD2',
+];
+
+export const isAdmin = (uid: string | undefined): boolean => {
+    if (!uid) return false;
+    return ADMIN_UIDS.includes(uid);
+}
