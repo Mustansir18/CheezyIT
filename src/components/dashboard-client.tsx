67cfafe015b4068dcff00630d90598a3e9236a5b
Mustansir Ali <mustansir133@gmail.com>
2026-01-21 14:22:54 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/components/dashboard-client.tsx
+++ src/components/dashboard-client.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import type { Ticket, TicketStatus } from '@/lib/data';
 import { getStats } from '@/lib/data';
 import { DateRange } from 'react-day-picker';
